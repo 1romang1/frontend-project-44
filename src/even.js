780297e697d43a4ca40a4a86f7a34c166f0abc37
1romang1 <krglrv@gmail.com>
2023-10-30 11:09:29 +0300

    fix Congratulation

--- src/even.js
+++ src/even.js
@@ -1,48 +1,46 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
+const evenGame = () => {
+  console.log('Welcome to the Brain Games!');
 
-const userName = readlineSync.question('May I have your name? ');
+  const userName = readlineSync.question('May I have your name? ');
 
-const greeting = () => {
-  console.log(`Hi ${userName}!`);
-};
+  const greeting = () => {
+    console.log(`Hi ${userName}!`);
+  };
 
-greeting();
+  greeting();
 
-const getRandomNum = (max) => Math.floor(Math.random() * max);
+  const getRandomNum = (max) => Math.floor(Math.random() * max);
 
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-for (let i = 0; i <= 2; i += 1) {
-  const randomNum = getRandomNum(100);
-  const randomNumQuestoinPhrase = `Question: ${randomNum} `;
-  const answer = readlineSync.question(randomNumQuestoinPhrase);
-  let correctAnswer = '';
-  if (randomNum % 2 === 0) {
-    correctAnswer = 'yes';
-  } else {
-    correctAnswer = 'no';
-  }
+  for (let i = 0; i <= 2; i += 1) {
+    const randomNum = getRandomNum(100);
+    const randomNumQuestoinPhrase = `Question: ${randomNum} `;
+    const answer = readlineSync.question(randomNumQuestoinPhrase);
+    let correctAnswer = '';
+    if (randomNum % 2 === 0) {
+      correctAnswer = 'yes';
+    } else {
+      correctAnswer = 'no';
+    }
 
-  console.log(`Your answer: ${answer}`);
+    console.log(`Your answer: ${answer}`);
 
-  if (
-    (randomNum % 2 === 0 && answer === 'yes')
-    || (randomNum % 2 !== 0 && answer === 'no')
-  ) {
-    console.log('Correct!');
-  } else {
-    // let oppositAnswer;
-    // if (answer === 'yes') {
-    //   oppositAnswer = 'no';
-    // } else {
-    //   oppositAnswer = 'yes';
-    // }
-    console.log(
-      `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. Let's try again, ${userName}`,
-    );
-    break;
+    if (
+      (randomNum % 2 === 0 && answer === 'yes')
+      || (randomNum % 2 !== 0 && answer === 'no')
+    ) {
+      console.log('Correct!');
+    } else {
+      console.log(
+        `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. Let's try again, ${userName}`,
+      );
+      break;
+    }
   }
   console.log(`Congratulations, ${userName}!`);
-}
+};
+
+export default evenGame;
