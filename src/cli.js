e8268918cf87a00b52fa83261515230d6c4a846c
Roma <krglrv@gmail.com>
2023-10-30 23:32:23 +0300

    fix: refactoring src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,13 +1,14 @@
-import { createRequire } from 'module';
-
-console.log('Welcome to the Brain Games!');
+import { createRequire } from "module";
 
 const require = createRequire(import.meta.url);
 
-const readlineSync = require('readline-sync');
+const readlineSync = require("readline-sync");
 
-const userName = readlineSync.question('May I have your name? ');
 const greeting = () => {
+  console.log("Welcome to the Brain Games!");
+
+  const userName = readlineSync.question("May I have your name? ");
+
   console.log(`Hi ${userName}!`);
 };
 
