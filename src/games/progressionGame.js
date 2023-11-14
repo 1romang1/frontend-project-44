081bd97c38759d19f126b09a62d42670110d843e
1romang1 <krglrv@gmail.com>
2023-11-14 12:22:06 +0300

    finish progression game

--- src/games/progressionGame.js
+++ src/games/progressionGame.js
@@ -1,42 +1,37 @@
-// import app from "../index.js";
+import app from "../index.js";
 
-// const TASK_PHRASE = "What number is missing in the progression?";
+const TASK_PHRASE = "What number is missing in the progression?";
 
 const generateTaskValuesProgression = () => {
   const values = [];
-
-  let correctAnswer;
-
   const MIN = 2;
   const MAX = 100;
 
   const APset = [];
   let startNum = Math.floor(Math.random() * (MAX - MIN) + MIN);
-  console.log(`startNum = ${startNum}`);
+
   APset.push(startNum);
-  console.log(`first action APset = ${APset}`);
 
   const differenceAP = Math.floor(Math.random() * (MAX - MIN) + MIN);
-  console.log(`differenceAP = ${differenceAP}`);
+
   for (let i = 0; i < 9; i += 1) {
     const nextAPNum = startNum + differenceAP;
     startNum = nextAPNum;
     APset.push(nextAPNum);
   }
-  console.log(`APset = ${APset[0]} ${APset[1]} ${APset[2]} ${APset[3]} ${APset[4]} ${APset[5]} ${APset[6]} ${APset[7]} ${APset[8]} ${APset[9]}`);
 
-const taskValue = APset[Math.floor(Math.random()*APset.length)];
-console.log(`taskValue = ${taskValue}`);
-  
+  const randomElement = APset[Math.floor(Math.random() * APset.length)];
 
-  //   correctAnswer =
-  //     
+  const indexOfElement = APset.indexOf(randomElement);
 
-  //   values.push(correctAnswer);
+  const correctAnswer = APset[indexOfElement];
+  APset[indexOfElement] = "..";
 
-  // return values;
+  const taskValue = APset.join(" ");
+  values.push(taskValue);
+  values.push(correctAnswer);
+
+  return values;
 };
 
-// export default () => app(TASK_PHRASE, generateTaskValuesProgression);
-
-generateTaskValuesProgression();
+export default () => app(TASK_PHRASE, generateTaskValuesProgression);
