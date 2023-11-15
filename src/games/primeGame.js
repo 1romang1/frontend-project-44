a2e453399d2a975fb3a530b97c167a488cd86528
1romang1 <krglrv@gmail.com>
2023-11-15 12:21:59 +0300

    prime game finish

---
+++ src/games/primeGame.js
@@ -0,0 +1,27 @@
+import app from "../index.js";
+
+const TASK_PHRASE =
+  'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const generateTaskValuesPrime = () => {
+  const FIRST_500_PRIME_NUMBERS = 3571;
+  const values = [];
+  const taskValue = Math.floor(Math.random() * FIRST_500_PRIME_NUMBERS);
+  values.push(taskValue);
+  let correctAnswer = "";
+  for (let i = 2; i < taskValue; i += 1) {
+    if (taskValue % i === 0) {
+      correctAnswer = "no";
+      break;
+    } else {
+      correctAnswer = "yes";
+    }
+  }
+  values.push(correctAnswer);
+  // console.log(values);
+  return values;
+};
+
+export default () => app(TASK_PHRASE, generateTaskValuesPrime);
+
+// generateTaskValuesPrime();
