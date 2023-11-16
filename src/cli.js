386f2d4f82411c04ae8ba4de62782af6393bc431
1romang1 <krglrv@gmail.com>
2023-11-16 12:50:38 +0300

    fix hexlet-check

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 console.log('Welcome to the Brain Games!');
 const userName = readlineSync.question('May I have your name? ');
 const greeting = () => {
-  console.log(`Hi ${userName}!`);
+  console.log(`Hello ${userName}!`);
 };
 
 export { userName };
