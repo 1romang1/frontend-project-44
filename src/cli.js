c50918e45d78006efcf1c2d04a81080abc22003e
1romang1 <krglrv@gmail.com>
2023-11-16 14:48:18 +0300

    fix hexlet-check 6

--- src/cli.js
+++ src/cli.js
@@ -2,9 +2,14 @@ import readlineSync from 'readline-sync';
 
 // console.log('Welcome to the Brain Games!');
 
+// const greeting = () => {
+//   const userName = readlineSync.question('May I have your name? ');
+//   console.log(`Hello ${userName}!`);
+// };
+
 const greeting = () => {
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello ${userName}!`);
+  // const userName = readlineSync.question('May I have your name? ');
+  console.log(`Welcome to the Brain Games!`);
 };
 
 
