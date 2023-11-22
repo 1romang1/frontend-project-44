a8a49103899277cf37ff557a7e523b758f2ebcdc
Roma <krglrv@gmail.com>
2023-11-22 21:02:04 +0300

    fix src/index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import readlineSync from "readline-sync";
 const app = (taskPhrase, generateTaskValues) => {
   console.log("Welcome to the Brain Games!");
   const userName = readlineSync.question("May I have your name? ");
+  console.log(`Hello, ${userName}`)
   console.log(`${taskPhrase}`);
 
-  // let i = 0;
   const iterationCounter = 3;
 
   for (let i = 0; i < iterationCounter; i += 1) {
