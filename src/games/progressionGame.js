901a7b3696d4248fbb528e0812a3068e9f6c2dc2
Roma <krglrv@gmail.com>
2023-11-23 21:20:59 +0300

    fix src/games/progressionGame.js

--- src/games/progressionGame.js
+++ src/games/progressionGame.js
@@ -3,23 +3,29 @@ import generatorHighLowRanges from '../generatorHighLowRanges.js';
 
 const TASK_PHRASE = 'What number is missing in the progression?';
 
+const createAP = (num, diffNum) => {
+  let result = [];
+  for (let i = 0; i < 9; i += 1) {
+    const nextAPNum = num + diffNum;
+    num = nextAPNum;
+    result.push(nextAPNum);
+  }
+  return result;
+}
+
 const generateTaskValuesProgression = () => {
   const values = [];
   const LOWER_RANGE = 2;
   const UPPER_RANGE = 100;
 
-  const APset = [];
+  let APset = [];
   let startNum = generatorHighLowRanges(UPPER_RANGE, LOWER_RANGE);
 
   APset.push(startNum);
 
   const differenceAP = generatorHighLowRanges(UPPER_RANGE, LOWER_RANGE);
-
-  for (let i = 0; i < 9; i += 1) {
-    const nextAPNum = startNum + differenceAP;
-    startNum = nextAPNum;
-    APset.push(nextAPNum);
-  }
+  const tempAPSet = createAP(startNum, differenceAP);
+APset = APset.concat(tempAPSet)
 
   const randomElement = APset[Math.floor(Math.random() * APset.length)];
 
