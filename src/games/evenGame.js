0c3bcc7f2c706729cf589c076f5949293d96aa98
Roma <krglrv@gmail.com>
2023-11-25 00:08:09 +0300

    fix isEven

--- src/games/evenGame.js
+++ src/games/evenGame.js
@@ -3,15 +3,7 @@ import generatRanges from '../generatRanges.js';
 
 const TASK_PHRASE = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEven = (value) => {
-  let result;
-  if (value % 2 === 0) {
-    result = 'yes';
-  } else {
-    result = 'no';
-  }
-  return result;
-};
+const isEven = (value) => (value % 2 === 0 ? 'yes' : 'no');
 
 const generateTaskValuesEven = () => {
   const UPPER_RANGE = 100;
