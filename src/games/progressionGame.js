f828f44d9fe9088206ee32f390542092bb466e39
1romang1 <krglrv@gmail.com>
2023-11-28 16:17:45 +0300

    fix hiddenElement

--- src/games/progressionGame.js
+++ src/games/progressionGame.js
@@ -2,38 +2,27 @@ import app from '../index.js';
 import generatRanges from '../generatRanges.js';
 
 const TASK_PHRASE = 'What number is missing in the progression?';
+const LOWER_RANGE = 2;
+const UPPER_RANGE = 100;
 
-const createAP = (num, diffNum) => {
+const createAp = () => {
   const result = [];
+  const startNum = generatRanges(UPPER_RANGE, LOWER_RANGE);
+  const differenceAp = generatRanges(UPPER_RANGE, LOWER_RANGE);
   const AMOUNT_OF_NUMBERS_IN_PROGRESSION = 9;
-  let nextAPNum = num;
+  let nextAPNum = startNum;
   for (let i = 0; i < AMOUNT_OF_NUMBERS_IN_PROGRESSION; i += 1) {
-    nextAPNum += diffNum;
+    nextAPNum += differenceAp;
     result.push(nextAPNum);
   }
   return result;
 };
 
 const generateTaskValuesProgression = () => {
-  const LOWER_RANGE = 2;
-  const UPPER_RANGE = 100;
-
-  let apSet = [];
-  const startNum = generatRanges(UPPER_RANGE, LOWER_RANGE);
-
-  apSet.push(startNum);
-
-  const differenceAP = generatRanges(UPPER_RANGE, LOWER_RANGE);
-  const tempapSet = createAP(startNum, differenceAP);
-  apSet = apSet.concat(tempapSet);
-
-  const randomElement = apSet[Math.floor(Math.random() * apSet.length)];
-
-  const indexOfElement = apSet.indexOf(randomElement);
-
-  const correctAnswer = apSet[indexOfElement];
-  apSet[indexOfElement] = '..';
-
+  const apSet = createAp();
+  const hiddenElement = apSet[generatRanges(apSet.length)];
+  const correctAnswer = hiddenElement;
+  apSet[apSet.indexOf(hiddenElement)] = '..';
   const taskValue = apSet.join(' ');
   return [taskValue, correctAnswer.toString()];
 };
